c7a06d6e2e5952d083d2a9b189583ae21efb4f67
nikballal <nikhil.ballal1994@gmail.com>
2022-06-09 16:43:48 +1000

    file setup & header component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/ui/Header";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
